148d6c64849a2ad94a2ba166d5dd5f9423924177
goodgear <100605263+goodgear@users.noreply.github.com>
2026-08-27 21:38:58 -0700

    Update home route message and run configuration

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Trillion is running."
+    return "Trillion online"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8000)
+
 
