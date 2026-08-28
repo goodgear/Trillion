318e9fd3648a51fc9b369a7a28f8f6f3a2543bef
goodgear <100605263+goodgear@users.noreply.github.com>
2026-08-27 21:34:28 -0700

    Refactor Flask app routes and remove predict endpoint
    Updated the Flask app to simplify the home route and removed the predict API endpoint.

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-from flask import Flask, render_template, jsonify
-from engines.mag import mag_engine
-from core.core import core_engine
-from armac.parse_armac import parse_armac
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def home():
+    return "Trillion is running."
 
-@app.route('/api/predict', methods=['GET'])
-def predict():
-    mag_signal = mag_engine()
-    core_signal = core_engine()
-    armac_data = parse_armac()
-    result = {
-        "mag_signal": mag_signal,
-        "core_signal": core_signal,
-        "armac_data": armac_data
-    }
-    return jsonify(result)
+if __name__ == "__main__":
+    app.run()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
